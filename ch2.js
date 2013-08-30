c20f21c07641054809881ba3174de0bdb166afcf
jrla <jimmy.lavado@outlook.com>
2013-08-29 20:08:48 -0500

    for a start

---
+++ ch2.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jrla
+ * Date: 29.08.13
+ * Time: 20:06
+ * To change this template use File | Settings | File Templates.
+ */
+/*
+ * This is a multi-line
+ * Comment
+ */
